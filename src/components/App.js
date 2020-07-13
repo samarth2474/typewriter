094612bcdd4df8a332c3cd3e3979edcd450da474
Samarth Mohapatra <samarthmmohapatra@gmail.com>
2020-07-13 12:08:24 +0530

    added src and public

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import SingleTest from './SingleTest'
+
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <SingleTest  />
+      </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/utils/dataStore.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export const MyContext = React.createContext()
+
+
+// used to store data that need to be accessed from differen parts of the app
+export default class MyProvider extends Component {
+  state = {
+    currentSample: "This is sample text",
+    setData: (data) => {return (
+        <div>
+          {this.state.currentSample}
+        </div>
+      )}
+  }
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    )
+
+  }
+}
